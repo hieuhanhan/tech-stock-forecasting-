78dbd7fb9a025aa6d13ca1c9a74d2c7d52c418a9
hhan <hanhieuhan@gmail.com>
2025-07-23 10:24:02 +0200

    Upload new script

--- resume_tuning_gru.sh
+++ resume_tuning_gru.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-# 1. Set simple variables
+# 1. Set simple variable
 S3_BUCKET="s3://tech-stock-data-2025/tuning_results"
-CHECKPOINT_FILE="final_moga_gru_results.json"
-PYTHON_SCRIPT="run_moga_optimization_gru.py"
+CHECKPOINT_FILE="final_moga_lstm_results.json"
+PYTHON_SCRIPT="run_moga_optimization_lstm.py"
 LOCAL_DIR="data/tuning_results"
+FOLDS_DIR="data/processed_folds"
 
-# 2. Read parameters
+# 2. Read parameter
 POP_SIZE=$1
 N_GEN=$2
 MAX_NEW_FOLDS=$3  
 
-# 3. Check parameters
+# 3. Check parameter
 if [ -z "$POP_SIZE" ] || [ -z "$N_GEN" ]; then
-  echo "Usage: ./resume_tuning_gru.sh <POP_SIZE> <N_GEN> [MAX_NEW_FOLDS]"
+  echo "Usage: ./resume_tuning_lstm.sh <POP_SIZE> <N_GEN> [MAX_NEW_FOLDS]"
   exit 1
 fi
 
-# 4. Make local dir if not exists
-mkdir -p $LOCAL_DIR logs
+# 4. Make dir
+mkdir -p $LOCAL_DIR $FOLDS_DIR logs
 START_TIME=$(date)
-LOG_FILE="logs/tuning_log_gru_$(date '+%Y%m%d_%H%M%S').log"
+LOG_FILE="logs/tuning_log_$(date '+%Y%m%d_%H%M%S').log"
 exec > >(tee -a "$LOG_FILE") 2>&1
 
-# 5. Pull checkpoint from S3
+# 5. Download checkpoint file
 echo "Pulling checkpoint from S3..."
 aws s3 cp "$S3_BUCKET/$CHECKPOINT_FILE" "$LOCAL_DIR/$CHECKPOINT_FILE"
+
+# 5.1 Download processed folds
+echo "Pulling processed folds from S3..."
+aws s3 sync s3://tech-stock-data-2025/processed_folds $FOLDS_DIR
 
 # 6. Start tuning
 echo "Running $PYTHON_SCRIPT with pop_size=$POP_SIZE, n_gen=$N_GEN, max_new_folds=${MAX_NEW_FOLDS:-ALL}..."
@@ -35,14 +40,14 @@ else
   python "$PYTHON_SCRIPT" --pop-size "$POP_SIZE" --n-gen "$N_GEN" --max-new-folds "$MAX_NEW_FOLDS"
 fi
 
-# 7. Check file
+# 7. Check script status
 PYTHON_EXIT_CODE=$?
 if [ $PYTHON_EXIT_CODE -ne 0 ]; then
-  echo "Python script failed with exit code $PYTHON_EXIT_CODE"
+  echo " Python script failed with exit code $PYTHON_EXIT_CODE"
   exit $PYTHON_EXIT_CODE
 fi
 
-# 7. Upload updated results to S3
+# 8. Upload results to S3
 echo "Uploading updated results to S3..."
 aws s3 cp "$LOCAL_DIR/$CHECKPOINT_FILE" "$S3_BUCKET/$CHECKPOINT_FILE"
 
